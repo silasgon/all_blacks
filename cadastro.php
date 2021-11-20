7841133068df9971242a02915a001441a70ec808
Silas Gonm <silasidc@gmail.com>
2021-11-20 13:21:21 -0300

    mod cad

--- cadastro.php
+++ cadastro.php
@@ -3,7 +3,7 @@
 include 'config.php';
 
 
-
+/* 
 $nome = $_POST['nome'];
 $documento = $_POST['documento'];
 $email = $_POST['email'];
@@ -12,15 +12,16 @@ $telefone = $_POST['telefone'];
 $cep = $_POST['cep'];
 $endereco = $_POST['endereco'];
 $cidade = $_POST['cidade'];
-$uf = $_POST['estado'];
+$uf = $_POST['uf'];
 $ativo = $_POST['ativo'];
 
-$recebendoDados = "INSERT INTO `torcedores`('nome', 'documento', 'email', 'ddd', 'telefone', 'cep', 'endereco', 'cidade', 'uf', 'ativo')
-VALUES('$nome', '$documento', '$email', '$ddd', '$telefone', '$cep', '$endereco', '$cidade', '$uf', '$ativo')";
+$recebendoDados = "INSERT INTO 
+torcedores
+VALUES('','$nome', '$documento', '$email', '$ddd', '$telefone', '$cep', '$endereco', '$cidade', '$uf', '$ativo')";
 $queryCadastro = mysqli_query($conexao, $recebendoDados);
+ */
 
-
-/* $nome = filter_input(INPUT_POST, 'nome');
+$nome = filter_input(INPUT_POST, 'nome');
 $documento = filter_input(INPUT_POST, 'documento');
 $email = filter_input(INPUT_POST, 'email');
 $ddd = filter_input(INPUT_POST, 'ddd');
@@ -31,9 +32,10 @@ $cidade = filter_input(INPUT_POST, 'cidade');
 $uf = filter_input(INPUT_POST, 'uf');
 $ativo = filter_input(INPUT_POST, 'ativo');
 
-$sql = $pdo->prepare('INSERT INTO torcedores (nome, documento, email, ddd, telefone, cep, endereco, cidade, uf, ativo)
- VALUES (:nome, :documento, :email, :ddd, :telefone, :cep, :endereco, :cidade, :uf, :ativo)');
+$sql = $pdo->prepare('INSERT INTO torcedores (id,nome, documento, email, ddd, telefone, cep, endereco, cidade, uf, ativo)
+ VALUES (:id,:nome, :documento, :email, :ddd, :telefone, :cep, :endereco, :cidade, :uf, :ativo)');
 
+ $sql->bindValue(':id', $id);
  $sql->bindValue(':nome', $nome);
  $sql->bindValue(':documento', $documento);
  $sql->bindValue(':email', $email);
@@ -44,8 +46,7 @@ $sql = $pdo->prepare('INSERT INTO torcedores (nome, documento, email, ddd, telef
  $sql->bindValue(':cidade', $cidade);
  $sql->bindValue(':uf', $uf);
  $sql->bindValue(':ativo', $ativo);
- $sql->execute(); */
-
+ $sql->execute();
 
  header('Location: index.php');
 ?>
